0549d5fa0c0c12b75c107176cb2b064c806b0cd4
Henry Farr <hfarr@hey.com>
2021-05-26 20:55:04 -0400

    Comment shuffle- some todos to todones, so removed those. added others.

--- src/index.js
+++ src/index.js
@@ -18,22 +18,17 @@ const app = express()
 const client = new discord.Client()
 const token = `${process.env.DISCORD_TOKEN}`
 
-const DB_HOST="redis" // docker bind
+const DB_HOST="redis" // docker bind - Parameterize this
 const redis = new Redis(DB_HOST);
 
-
-// One thing we can use oauth for is verification - logging into the web panel with a valid OAUTH token
+// One thing we can use oauth for is verification - logging into the web panel with a valid OAUTH token (with identity scope, likely)
 // then only people with the correct permission can login.
 
-const joined_guilds = []
-const guilds = {}
-const webhooks = {}
-
+// This link lets someone register their bot to a server, and set up a webhook. 
+// TODO gracefully handle cases where necessary permissions are not granted.
 const CURRENT_LINK = "https://discord.com/api/oauth2/authorize?client_id=846454323856408636&permissions=536939520&redirect_uri=https%3A%2F%2Flinguine.hfarr.net%2Fapi%2Ftoken&response_type=code&scope=bot%20webhook.incoming"
 
 app.get('/', (req, res) => {
-    //     res.send(`<p>Register a webhook</p>
-    // <a href=https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=https%3A%2F%2Flinguine.hfarr.net%2Fapi%2Ftoken&response_type=code&scope=messages.read>register token</a>`)
     res.send(`<p>Register a bot</p>
 <a href=${CURRENT_LINK}>register webhook</a>`)
 
@@ -50,7 +45,7 @@ app.get('/no', (req, res) => {
 const API_ENDPOINT = 'https://discord.com/api/oauth2/token'
 const CLIENT_ID = `${process.env.CLIENT_ID}`
 const CLIENT_SECRET = `${process.env.CLIENT_SECRET}`
-// might have to use 
+// might have to use. Im not 100% positive what happens when you give a different redirect to the one used for code granting.
 const REDIRECT_URI = 'https://linguine.hfarr.net/api/token'
 
 // returns a promise, yay async
@@ -110,14 +105,12 @@ app.get('/api/token', (req, res) => {
 })
 
 
-// TODO add to persistent DB
 function new_registration(info) {
-    // TODO save info to file as json string
+
     let { access_token, refresh_token, token_type, guild, webhook } = info
 
     // how we'll identify the bot
     let guild_id = guild["id"]
-    joined_guilds.push(guild_id)
 
     let guild_info = {
         guild_id: guild_id,
@@ -125,25 +118,23 @@ function new_registration(info) {
         refresh_token: refresh_token,
         token_type: token_type,
     }
-    guilds[guild_id] = guild_info   // TODO save to a db
 
     // only saving one webhook to a guild for this bot, so we won't make it a list
     let { id, token, channel_id } = webhook
     let hook = { id, token, channel_id }
-    webhooks[guild_id] = hook
 
     // save in db
     redis.pipeline()
-        .rpush('joined_guilds', guild_id)
+        .rpush('joined_guilds', guild_id)                       // All guilds to which this bot has ever been added. Take note, if the bot is removed, it is NOT removed from this list - for now
         .set(`discordInfo:${JSON.stringify(info)}`)             // Original response from discord in case we need to recover state
         .set(`guilds:${guild_id}`, JSON.stringify(guild_info))  // TODO use a redis hash, or zmap or whatever
         .set(`webhooks:${guild_id}`, JSON.stringify(hook))      // yeah. storing JSON strings... not the best?
-        .exec((err, results) => {})                             // todo error handling. lots of work pushed off today..
+        .exec((err, results) => {})                             // TODO error handling. lots of work pushed off today..
 
 }
 
 // This only applies to numbers, since the automatic default Im DECLARING to be 0.
-// This is a good case for generics. Other types might have default values, so if v and dv are 
+// This is a good case for generics. Other types might have default values of their type, so if v and dv are 
 // constrained to the same type, then I can apply the default value of the type for dv if it 
 // goes unspecified. Moreover we can specify a "constraint" predicate rather than checking if v is not null, is not undefined
 // This function returns v unless it is null or undefined, then it returns a default value. Numerics only (for now)
@@ -154,6 +145,7 @@ const compose = (f,g) => x => f(g(x))   // Im glad the notation is flexible lol,
 // Small utility function. 4am is when points expire. To implement this we use redis
 // key expiration. After the key expires, if we try to read it, we'll get a default
 // of 0 as we interpret no points stored as no points whatsoever.
+// I should publish this as a package on the NPM registry haha
 function secondsTill4am() {
     // Lots of string manipulation to arrive at what we want, possible room for optimization.
     const UTC_RESET_HOUR = 8
@@ -168,14 +160,14 @@ function secondsTill4am() {
     }
     // if, for some reason, we cross the 4AM threshold before finishing this function, then we don't want to
     // replace 'now' with a newly constructed date, because that would but 'now' after 'resetTime'.
-    // Just poor luck for the sap that earned points.
+    // Just poor luck for the sap that earned points. Highly unlikely - famous last words
     return date.subtract(resetTime, now).toSeconds()
 }
 
-// TODO need to get a graps on Promise fundamentals, and how 'then', 'catch', etc. are implemented
+// TODO need to get a graps on Promise fundamentals, and how 'then', 'catch', etc. are implemented. Rough idea atm
 // Retrieves guild information from redis-
 //  note that I'm constructing basically a jank object. Really should use the class semantics.
-//  also note this returns a promise
+//  note this returns a promise
 function getGuildInfo(guildID) {
     const guildKey = `guilds:${guildID}`
     const promise = redis.get(guildKey)
@@ -207,7 +199,8 @@ function getGuildInfo(guildID) {
 }
 
 /**
- * Fetch the User within the guild, or null if such a user doesn't exist.
+ * Get the corresponding GuildMember instance for a user's snowflake, 
+ * within the guild, or null if such a user doesn't exist.
  * @param {*} guild_id Snowflake of guild
  * @param {*} user_id Snowflake of user
  * @returns GuildMember object referring to user with id
@@ -234,31 +227,15 @@ function send_message(guild_id, message) {
         .catch(console.error)
 }
 
+// Parse an ID from user mention in discord, which wraps it in angle brackets and varying 
+// other symbols (I've seen !, &, and plain- not sure exactly what they indicate)
 function parse_user_mention(mention_string) {
     const user_match = /\D*(?<id>\d+).*/
     let { groups: { id: result = null } = {} } = user_match.exec(mention_string) ?? {}
     return result
 }
 
-
-// TODO should register these functions into guild_info on start up, or when 
-// read from a database. In other words instantiate an object properly, why don't we... :S
-// (comment taken from below) TODO data base op, and when reading from database, querying you might say, it will default to 0 instead of this nonsense
-function get_points(guild_info, user_id) {
-    // Fetch users' points. If they're data isn't tracked, default to 0.
-    let { points: current_points = 0 } = guild_info['user_data'][user_id] ?? {}
-    // let { user_id: { points: current_points = 0 } = {} } = guild_info['user_data'] // p-sure this line does the same as the above, advanced destructuring! but less clean.
-    // lets be real, neither are clean, this is not clear code but I like it so it stays until I upgrade to classes
-    return current_points
-}
-function get_linguines(guild_info, user_id) {
-    // Fetch users' points. If they're data isn't tracked, default to 0.
-    let { linguines: current_linguines = 0 } = guild_info['user_data'][user_id] ?? {}
-    return current_linguines 
-}
-
-// TODO another function to make into a database op
-function add_points(guild_id, user_id, points) { // todo should check points > 0, or have that as a DB constraint or something.
+function add_points(guild_id, user_id, points) {
 
     let guildInfo 
     return getGuildInfo(guild_id)
@@ -277,8 +254,8 @@ function add_points(guild_id, user_id, points) { // todo should check points > 0
         })
 }
 
-function add_linguines(guild_id, user_id, linguines = 1) { // todo should check linguines > 0, or have it as a DB constraint. Golly.
-    let guildInfo   // maybe create syntax for "with" scopes, so you can specify variables available in all promises, or bind results for every pipelined function?
+function add_linguines(guild_id, user_id, linguines = 1) {
+    let guildInfo   // maybe create syntax for "with" scopes, so you can specify variables available in all promises, or bind results available to succeeding pipelined function?
     return Promise.resolve()
         .then(() => getGuildInfo(guild_id))
         .then(gi => { guildInfo = gi; return guildInfo.getLinguines(user_id) })
@@ -289,11 +266,10 @@ function add_linguines(guild_id, user_id, linguines = 1) { // todo should check 
 function points_command(msg, [user, points_str]) {
 
     let guild_id = msg.guild.id
-    // let guild_info = guilds[guild_id]   // TODO database op to retrieve! async
 
     // arguments in the deconstruction are either strings or undefined, unless someone pulls a big prank. in a module it wouldnt be an issue :eye_roll: because this code would be hidden
-    if (user === undefined) { // no second argument to !points, so get current usr points
-        user = msg.author.id    // should still get parsed
+    if (user === undefined) { // no argument to !points, so pretend the requesting user passed themselves as an argument
+        user = msg.author.id
     }
 
     // Ive found that in substr using offset of 0 and 1 back from length yield the same string! seems inconsistent.
